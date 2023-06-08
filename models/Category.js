2085f13600e634c332d4ca19d000aaa1192fa71b
Steven Robertson <s2robertson@gmail.com>
2023-06-08 19:04:59 -0400

    added fields to category model

--- models/Category.js
+++ models/Category.js
@@ -2,19 +2,24 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
-Category.init(
-  {
-    // define columns
-  },
-  {
+Category.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
-);
+});
 
 module.exports = Category;
