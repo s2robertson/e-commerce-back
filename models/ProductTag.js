60b097126faec56a0fcfdc339aa794f144bc0e2e
Steven Robertson <s2robertson@gmail.com>
2023-06-08 19:35:57 -0400

    added fields to product_tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
+ProductTag.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Product,
+            key: 'id'
+        }
+    },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Tag,
+            key: 'id'
+        }
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
-);
+});
 
 module.exports = ProductTag;
