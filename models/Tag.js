e4203e86261362989ea43af51302aae46fbdc87b
Steven Robertson <s2robertson@gmail.com>
2023-06-08 19:20:04 -0400

    added fields to tag model

--- models/Tag.js
+++ models/Tag.js
@@ -2,19 +2,21 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
-Tag.init(
-  {
-    // define columns
-  },
-  {
+Tag.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    tag_name: DataTypes.STRING
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'tag',
-  }
-);
+});
 
 module.exports = Tag;
