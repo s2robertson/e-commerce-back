8709bbecd88a74dac48cda72c8afc80272148de6
Steven Robertson <s2robertson@gmail.com>
2023-06-08 20:12:22 -0400

    adjusted foreign key in category -> product association

--- models/index.js
+++ models/index.js
@@ -6,11 +6,13 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+    foreignKey: 'category_id'
 });
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag });
@@ -19,8 +21,8 @@ Product.belongsToMany(Tag, { through: ProductTag });
 Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
